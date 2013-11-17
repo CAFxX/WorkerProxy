282e5933f2b4b6218864ba66aa39d5ad8f3f5282
Carlo Alberto Ferraris <cafxx@strayorange.com>
2013-11-17 14:27:02 +0100

    more tests

--- test/test.js
+++ test/test.js
@@ -1,5 +1,3 @@
-var delay = 200;
-
 asyncTest("Empty WorkerProxy", 4, function() {
   var wp = new WorkerProxy(null, function() {
     ok( wp === this, "wp passed to callback as this" );
@@ -30,22 +28,40 @@ asyncTest("Remote eval (function)", 1, function() {
   });
 });
 
-asyncTest("Simple RPC (automatic enumeration))", 2, function() {
+asyncTest("Simple RPC (automatic enumeration)", 2, function() {
   var wp = new WorkerProxy('test-worker.js', function() {
     ok( 'foo' in wp, 'foo is present' );
     wp.foo(5).done(function(res) {
-      equal(res, 6, "remote eval success");
+      equal(res, 6, "remote call success");
       start();
     });
   });
 });
 
-asyncTest("Simple RPC (explicit enumeration))", 3, function() {
+asyncTest("Simple RPC (parallel calls)", 1, function() {
+  var wp = new WorkerProxy('test-worker.js', function() {
+    var defs = [], success = 0;
+    for (var i=0; i<1000; i++) {
+      (function(i) {
+        var def = wp.foo(i).done(function(res) {
+          if (res === i+1) success++;
+        });
+        defs.push(def);
+      })(i);
+    }
+    $.when.apply(null, defs).then(function() {
+      equal(defs.length, success, "parallel remote calls success");
+      start();
+    })
+  });
+});
+
+asyncTest("Simple RPC (explicit enumeration)", 3, function() {
   var wp = new WorkerProxy('test-worker-exports.js', function() {
     ok( 'foo' in wp, 'foo is present' );
     ok( 'bar' in wp === false, 'bar is not present' );
     wp.foo(5).done(function(res) {
-      equal(res, 6, "remote eval success");
+      equal(res, 6, "remote call success");
       start();
     });
   });
